1f073c4a8638935bc2567eb3a57ecd89fd041ca9
Ademola Abdulkareem <dr.white004@gmail.com>
2025-01-17 13:08:28 +0100

    requiremts added

--- users/admin.py
+++ users/admin.py
@@ -2,9 +2,8 @@ from django.contrib import admin
 from .models import UserProfile
 from django.contrib.auth.models import User
 
-# Register UserProfile model
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'email_verified')  # Customize list display
+    list_display = ('user', 'email_verified')  
     search_fields = ('user__username', 'user__email')  # Add search fields
-    list_filter = ('email_verified',)  # Add filters
+    list_filter = ('email_verified',)  
